404d97dfa609442a24cefa4f5698f3f1f6f8f88b
Himanshu87504 <165474672+Himanshu87504@users.noreply.github.com>
2026-04-11 15:15:21 +0530

    Fix render routing

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -9,7 +9,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:4000/",
+    baseURL: "https://resume-backend-5uua.onrender.com/",
 });
 
 export default instance;
